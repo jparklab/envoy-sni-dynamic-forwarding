7b851ad43d9651173bc8d6e2ace37caff290e4e4
Christoph Pakulski <paker8848@gmail.com>
2025-10-24 19:51:30 +0000

    Extract and display SNI.
    Signed-off-by: Christoph Pakulski <paker8848@gmail.com>

--- wasm-plugin-1/src/lib.rs
+++ wasm-plugin-1/src/lib.rs
@@ -3,6 +3,7 @@ use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
 //use std::time::Duration;
 use proxy_wasm::hostcalls::call_foreign_function;
+use proxy_wasm::hostcalls::get_property;
 use prost::Message;
 
 pub mod wasm_extensions {
@@ -20,6 +21,37 @@ struct GrpcAuthRandom;
 impl StreamContext for GrpcAuthRandom {
     fn on_new_connection(&mut self) -> Action {
     log::info!("On NEW connection called");
+
+
+    // Get SNI from downstream connection.
+    let path = vec!["connection", "requested_server_name"];
+
+    match get_property(path) {
+        Ok(sni) => {
+            log::info!("Found SNI");
+            match sni {
+                Some(s) => {
+                    log::info!("There is a vector in SNI option");
+                    if s.is_empty() {
+                        log::info!("SNI vector is empty");
+                    } else {
+                        log::info!("SNI vector is NOT empty");
+            let s = match str::from_utf8(&s) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+            log::info!("SNI of requested server name is: {}", s);
+                    }
+                },
+                None => {
+                    log::info!("There is NO vector in SNI option");
+                },
+            };
+/*
+*/
+        },
+        Error => {log::info!("NOT found SNI");},
+    }; 
 
     let dynamic_port = wasm_extensions::SetEnvoyFilterStateArguments{    
         path: "envoy.upstream.dynamic_port".to_string(),
