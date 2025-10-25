5162170df568efa839d91f6bfb44b9ab46368ee4
Christoph Pakulski <paker8848@gmail.com>
2025-10-25 02:17:40 +0000

    Moved setting filter state to a function.
    Signed-off-by: Christoph Pakulski <paker8848@gmail.com>

--- wasm-plugin-1/src/lib.rs
+++ wasm-plugin-1/src/lib.rs
@@ -1,7 +1,7 @@
 use log::info;
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
-//use std::time::Duration;
+use std::time::Duration;
 use proxy_wasm::hostcalls::call_foreign_function;
 use proxy_wasm::hostcalls::get_property;
 use prost::Message;
@@ -53,6 +53,9 @@ impl StreamContext for GrpcAuthRandom {
         Error => {log::info!("NOT found SNI");},
     }; 
 
+
+    // When this is called from on_new_connection it sets metadata.
+/*
     let dynamic_port = wasm_extensions::SetEnvoyFilterStateArguments{    
         path: "envoy.upstream.dynamic_port".to_string(),
         value: "19443".to_string(),
@@ -75,10 +78,93 @@ impl StreamContext for GrpcAuthRandom {
     buf.reserve(dynamic_host.encoded_len());
     dynamic_host.encode(&mut buf).unwrap();
     let _ = call_foreign_function("set_envoy_filter_state", Some(&buf));
+*/
+        self.setUpstream();
 
-                //return Action::Pause;
-                Action::Continue
+          self.dispatch_http_call(
+            "httpbin",
+            vec![
+                (":method", "GET"),
+                (":path", "/bytes/1"),
+                (":authority", "127.0.0.1"),
+            ],
+            None,
+            vec![],
+            Duration::from_secs(1),
+        )
+        .unwrap();
+        Action::Pause
     }
 }
 
-impl Context for GrpcAuthRandom {}
+    impl GrpcAuthRandom { 
+    fn setUpstream(&mut self) {
+    let dynamic_port = wasm_extensions::SetEnvoyFilterStateArguments{    
+        path: "envoy.upstream.dynamic_port".to_string(),
+        value: "19443".to_string(),
+        // TODO: change it to enum
+        span: 0 /*LifeSpan::FilterChain */
+    };
+    let mut buf = Vec::new();
+    buf.reserve(dynamic_port.encoded_len());
+    dynamic_port.encode(&mut buf).unwrap();
+    match call_foreign_function("set_envoy_filter_state", Some(&buf))
+    {
+        Ok(_) => {
+            log::info!("Foreign function OK");
+        },
+        Err(_) => {
+            log::info!("Foreign function ERROR");
+        },
+    }
+	
+    let dynamic_host = wasm_extensions::SetEnvoyFilterStateArguments{    
+        path: "envoy.upstream.dynamic_host".to_string(),
+        value: "127.0.0.1".to_string(),
+        // TODO: change it to enum
+        span: 0 /*LifeSpan::FilterChain */
+    };
+
+    buf.clear();
+    buf.reserve(dynamic_host.encoded_len());
+    dynamic_host.encode(&mut buf).unwrap();
+    let _ = call_foreign_function("set_envoy_filter_state", Some(&buf));
+    }
+}
+
+
+impl Context for GrpcAuthRandom {
+    fn on_http_call_response(&mut self, _: u32, _: usize, body_size: usize, _: usize) {
+            log::info!("RECEIVED HTTP CALL RESPONSE");
+/*
+    let dynamic_port = wasm_extensions::SetEnvoyFilterStateArguments{    
+        path: "envoy.upstream.dynamic_port".to_string(),
+        value: "19443".to_string(),
+        // TODO: change it to enum
+        span: 0 /*LifeSpan::FilterChain */
+    };
+    let mut buf = Vec::new();
+    buf.reserve(dynamic_port.encoded_len());
+    dynamic_port.encode(&mut buf).unwrap();
+    let _ = call_foreign_function("set_envoy_filter_state", Some(&buf));
+	
+    let dynamic_host = wasm_extensions::SetEnvoyFilterStateArguments{    
+        path: "envoy.upstream.dynamic_host".to_string(),
+        value: "127.0.0.1".to_string(),
+        // TODO: change it to enum
+        span: 0 /*LifeSpan::FilterChain */
+    };
+
+    buf.clear();
+    buf.reserve(dynamic_host.encoded_len());
+    dynamic_host.encode(&mut buf).unwrap();
+    let _ = call_foreign_function("set_envoy_filter_state", Some(&buf));
+*/
+        //self.setUpstream();
+                //return Action::Pause;
+                //Action::Continue
+                self.resume_downstream();
+                //self.resume_upstream();
+                return;
+    }
+}
