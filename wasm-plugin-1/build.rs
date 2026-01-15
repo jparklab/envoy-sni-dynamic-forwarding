f7ee582681e4bd2a904abc9f14c7f8f70c7684d0
Christoph Pakulski <paker8848@gmail.com>
2026-01-15 16:49:25 +0000

    Removed hardcoded Envoy dir. It is found via env variable ENVOY_DIR.
    Signed-off-by: Christoph Pakulski <paker8848@gmail.com>

--- wasm-plugin-1/build.rs
+++ wasm-plugin-1/build.rs
@@ -1,9 +1,15 @@
+use std::env;
 use std::io::Result;
+
 fn main() -> Result<()> {
-    print!("Compiling\n");
+    print!("Compiling protos\n");
+    env::var("ENVOY_DIR")
+        .expect("Environment variable ENVOY_DIR not set. It is needed to find WASM protos.");
+
+    let root_dir = env::var("ENVOY_DIR").unwrap() + "/";
     prost_build::compile_protos(
-        &["/home/christoph/envoy/source/extensions/common/wasm/ext/set_envoy_filter_state.proto"],
-        &["/home/christoph/envoy/"],
+        &[root_dir.clone() + "source/extensions/common/wasm/ext/set_envoy_filter_state.proto"],
+        &[root_dir],
     )?;
     Ok(())
 }
